bfd83b695591d590e42bba03c545b23a848cb6e5
Noah Lindsey <47871969+7Gaming@users.noreply.github.com>
2020-09-05 15:49:24 -0400

    Vars.env
    A new file "vars.env" should be placed in the root directory and hold all secret keys

--- COVIDAway/settings.py
+++ COVIDAway/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.getcwd(), 'vars.env'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '91&ykrrt&+_7tl_*g#4*k-=b6+3$dvr_q^cr!%g*jyz*vv05$z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
