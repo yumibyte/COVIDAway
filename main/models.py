e491fe8bd745fc4d9a603f85d0bfb148bd4cc296
Noah Lindsey <47871969+7Gaming@users.noreply.github.com>
2020-09-06 01:36:56 -0400

    Database
    Data entered on the front end now syncs with the backend

--- main/models.py
+++ main/models.py
@@ -10,11 +10,12 @@ class Individual(models.Model):
     need_faceshields = models.BooleanField(default=False)
     need_facemasks = models.BooleanField(default=False)
     notes = models.TextField(max_length=5000, default="Default desc.")
-    long = models.DecimalField(max_digits=9, decimal_places=6, default=0)
     lat = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    long = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
-        return str([self.name, self.email, self.people, [self.need_gloves, self.need_faceshields, self.need_facemasks], self.notes, self.long, self.lat])
+        return str([self.time.strftime("%m/%d/%Y - %H:%M"), self.name, self.email, self.people, [self.need_gloves, self.need_faceshields, self.need_facemasks], self.notes, self.lat, self.long])
 
 #from main.models import Individual
 #i = Individual(name='Jerry Smith', email='jerrybear@gmail.com', people=4, need_facemasks=True)
--- main/views.py
+++ main/views.py
@@ -1,20 +1,70 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import View
+from main.models import Individual
 
 # Create your views here.
 
 class MainView(View):
     #Establish a dictionary that will be sent to the template html
-    context = { }
+    #context = { }
 
     #Used to render a page
-    def load_page(self, request, page):
+    def load_page(self, request, page, context = {}):
         print("New webpage request (" + page + ")")
-        return render(request, page, self.context)
+        return render(request, page, context)
+
+    def check_sub(self, submission):
+        #SUBMISSION DICTIONARY - name, email, people, need_gloves, need_faceshields, need_facemasks, notes
+        print("Checking...")
+        #if submission['lat'] is not "" and submission['long'] is not "":
+            #print("Coords passed")
+        if submission['name'].replace(" ", "").isalpha() and len(submission['name']) < 255:
+            print("Name passed")
+            if submission['email'].replace("@", "").replace(".","").isalnum() and "@" in submission['email'] and "." in submission['email'].split("@")[1] and len(submission['email']) < 255:
+                print("Email passed")
+                if submission['people'].isdigit() and int(submission['people']) in range(1, 10000): #people must be < 10,000
+                    print("People passed")
+                    if 'need_gloves' not in submission or 'need_faceshields' not in submission or 'need_facemasks' not in submission:
+                        print("PPE passed")
+                        if len(submission['notes']) < 5000:
+                            print("Notes passed")
+                            self.write_individual(submission)
+                            return True
+        return False
+
+    def write_individual(self, submission):
+        i = Individual(name=submission['name'], email=submission['email'], people=int(submission['people']), lat=float(submission['lat']), long=float(submission['long']), time=timezone.now())
+
+        if 'need_gloves' in submission:
+            i.need_gloves = True
+        if 'need_faceshields' in submission:
+            i.need_faceshields = True
+        if 'need_facemasks' in submission:
+            i.need_facemasks = True
+        if len(submission['notes']) > 0:
+            i.notes = submission['notes']
+
+        i.save()
+        return
 
     #When a user goes to the page without posting data
     def get(self, request):
         return self.load_page(request, 'requestppe.html')
+
+    #When the user posts data to the server
+    def post(self, request):
+        print("-----")
+        print(request.POST)
+        print("-----")
+
+        text = "Whoops! Something's gone wrong!"
+        if self.check_sub(request.POST):
+            text = "Your information has been submitted successfully!"
+
+        return self.load_page(request, 'result.html', {'text': text})
+
+
 
 '''
     #When a user goes to the page without posting data
